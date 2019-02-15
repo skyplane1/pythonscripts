f33f4aeb258407160073865b6c32a92a0be96412
prajay <colosseus6194@gmail.com>
2019-02-15 23:17:43 +0530

    Made script use functions

--- counting even and odd no.py
+++ counting even and odd no.py
@@ -1,14 +1,19 @@
 str1 = (input('enter the terms with a space you want in the list'))
-list1 = str1.split()
-print(list1)
-even = 0
-odd = 0
-for i in list1:
-    num = int(i)
-    if num % 2 == 0:
-        even += 1
-    else:
-        odd += 1
 
-print('the no. of even no is', even ,'the no of odd no is ', odd)
+def count(string):
+
+    list1 = string.split()
+    even = 0
+    odd = 0
+    for i in list1:
+        num = int(i)
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    print('the no. of even no is', even ,'the no of odd no is ', odd)
+
+
+
+count (str1)
 
